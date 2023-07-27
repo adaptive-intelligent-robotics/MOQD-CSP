68408b34d3b489d96bbc2159127a9ce3ff5405f9
Marta <marta.wolinska16@imperial.ac.uk>
2023-07-27 01:28:27 +0100

    pyximport + batching

--- run_experiment.py
+++ run_experiment.py
@@ -8,7 +8,8 @@ from ase.ga.utilities import CellBounds
 
 from csp_elites.utils.asign_target_values_to_centroids import \
     compute_centroids_for_target_solutions, reassign_data_from_pkl_to_new_centroids
-from csp_elites.crystal.crystal_evaluator import CrystalEvaluator, MaterialProperties
+from csp_elites.crystal.crystal_evaluator import CrystalEvaluator
+from csp_elites.crystal.materials_data_model import MaterialProperties
 from csp_elites.crystal.crystal_system import CrystalSystem
 from csp_elites.map_elites.cvt_csp import CVT
 from csp_elites.utils.experiment_parameters import ExperimentParameters
@@ -20,22 +21,24 @@ from csp_elites.utils.plot import load_archive_from_pickle, load_centroids, \
 from csp_elites.map_elites.elites_utils import make_current_time_string, __centroids_filename
 
 if __name__ == '__main__':
+    import pyximport
+    pyximport.install(setup_args={"script_args": ["--verbose"]})
     os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     experiment_parameters = ExperimentParameters(
         number_of_niches=200,
         maximum_evaluations=100,
-        experiment_tag="100_evals_cghnet",
+        experiment_tag="test",
         fitler_comparison_data_for_n_atoms=24,
         cvt_run_parameters= \
             {
                 # more of this -> higher-quality CVT
                 "cvt_samples": 25000,
                 # we evaluate in batches to paralleliez
-                "batch_size": 10,
+                "batch_size": 20,
                 # proportion of niches to be filled before starting
-                "random_init": 0.05,
+                "random_init": 0.1,
                 # batch for random initialization
-                "random_init_batch": 10,
+                "random_init_batch": 0,
                 # when to write results (one generation = one batch)
                 "dump_period":  10,
                 # do we use several cores?
@@ -46,9 +49,10 @@ if __name__ == '__main__':
                 "bd_minimum_values": (0, 0),
                 "bd_maximum_values": (100, 100),
                 "relaxation_probability": 0,
-                "behavioural_descriptors": [MaterialProperties.SHEAR_MODULUS, MaterialProperties.BAND_GAP],
-                "number_of_relaxation_steps": 10,
+                "behavioural_descriptors": [MaterialProperties.BAND_GAP, MaterialProperties.SHEAR_MODULUS],
+                "number_of_relaxation_steps": 0,
                 "curiosity_weights": True,
+                "filter_starting_Structures": 24,
             },
         system_name="TiO2",
         blocks = [22] * 8 + [8] * 16,
@@ -58,10 +62,10 @@ if __name__ == '__main__':
         cellbounds = CellBounds(
             bounds={'phi': [20, 160], 'chi': [20, 160], 'psi': [20, 160], 'a': [2, 40], 'b': [2, 40],
                     'c': [2, 40]}),
-        operator_probabilities=[5., 1, 2., 2.],
+        operator_probabilities=[5., 0, 2., 3.],
         ### CVT PARAMETERS ###
         n_behavioural_descriptor_dimensions=2,
-        fitness_min_max_values=[4, 9],
+        fitness_min_max_values=[0, 10],
     )
 
     print(experiment_parameters)
@@ -92,7 +96,7 @@ if __name__ == '__main__':
         crystal_evaluator=crystal_evaluator,
     )
     tic = time.time()
-    experiment_directory_path, archive = cvt.compute(
+    experiment_directory_path, archive = cvt.batch_compute_with_list_of_atoms(
         number_of_niches=experiment_parameters.number_of_niches,
         maximum_evaluations=experiment_parameters.maximum_evaluations,
         run_parameters=experiment_parameters.cvt_run_parameters,
