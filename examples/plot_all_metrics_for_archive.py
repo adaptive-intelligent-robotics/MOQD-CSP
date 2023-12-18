fd7f75ef70f1c7e13544727e1fede3a4b4a527f5
hannah <hnj21@ic.ac.uk>
2023-12-18 15:39:13 +0000

    remove redundant scripts and update .ignore

--- examples/plot_all_metrics_for_archive.py
+++
@@ -1,5 +0,0 @@
-from retrieve_results.handy_method_wrappers import plot_metrics_for_one_folder
-
-if __name__ == '__main__':
-    folder = "20230826_22_59_TiO2_200_niches_for_benchmark_100_relax_4"
-    plot_metrics_for_one_folder(folder)
